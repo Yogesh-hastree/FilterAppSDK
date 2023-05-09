14fa3e1627c077635d1e198c118ad8a8b3e5bf39
yogesh-hastree <yogeshkumar.hastree@gmail.com>
2023-05-09 18:15:07 +0530

    remove init() code

--- Sources/FilterAppSDK/FilterAppSDK.swift
+++ Sources/FilterAppSDK/FilterAppSDK.swift
@@ -2,7 +2,7 @@ public struct FilterAppSDK {
     public private(set) var text = "Hello, World!"
 
     public init() {
-        print("Hello")
+//        print("Hello")
     }
     
     public func msg(str: String){
