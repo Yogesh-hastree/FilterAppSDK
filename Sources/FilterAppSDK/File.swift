929cc63fdd92e6ee92edb3f92058c6576f9ad99c
yogesh-hastree <yogeshkumar.hastree@gmail.com>
2023-05-09 18:07:20 +0530

    make Class and Function Public

---
+++ Sources/FilterAppSDK/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hastree on 09/05/23.
+//
+
+import Foundation
+
+public class File{
+    
+    public func massage(title: String, discription: String){
+        print(title)
+        print(discription)
+    }
+}
--- Sources/FilterAppSDK/FilterAppSDK.swift
+++ Sources/FilterAppSDK/FilterAppSDK.swift
@@ -2,5 +2,10 @@ public struct FilterAppSDK {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        print("Hello")
+    }
+    
+    public func msg(str: String){
+        print(str)
     }
 }
